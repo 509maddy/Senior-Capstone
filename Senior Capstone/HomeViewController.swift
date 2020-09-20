81467ac41dd330912b07a454044893a13bcbb0bc
Ally Dinhofer <ally.dinhofer@gmail.com>
2020-09-20 11:11:34 -0400

    adding pie chart info with dummy data

--- Senior Capstone/HomeViewController.swift
+++ Senior Capstone/HomeViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import Charts
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, ChartViewDelegate {
 
+    var pieChart = PieChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pieChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pieChart.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        pieChart.center = view.center
+        view.addSubview(pieChart)
+        
+        var entries = [ChartDataEntry]()
+        
+        for x in 0..<10 {
+            entries.append(ChartDataEntry(x: Double(x), y: Double(x)))
+        }
+        let set = PieChartDataSet(entries : entries)
+        set.colors = ChartColorTemplates.colorful()
+        let data = PieChartData(dataSet: set)
+        pieChart.data = data
     }
     
 
