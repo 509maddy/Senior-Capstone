407bcbbf27af622f33b9bd658c1617d97e6267c4
Ally Dinhofer <ally.dinhofer@gmail.com>
2020-09-22 21:04:29 -0400

    added all 5 pie charts
    Signed-off-by: Ally Dinhofer <ally.dinhofer@gmail.com>

--- Senior Capstone/HomeViewController.swift
+++ Senior Capstone/HomeViewController.swift
@@ -11,27 +11,54 @@ import Charts
 
 class HomeViewController: UIViewController, ChartViewDelegate {
 
-    var pieChart1 = PieChartView()
-    var pieChart2 = PieChartView()
+    // creating each pie chart
+    var fruitChart = PieChartView()
+    var meatChart = PieChartView()
+    var grainsChart = PieChartView()
+    var dairyChart = PieChartView()
+    var vegChart = PieChartView()
     
+    // connecting to each view
     @IBOutlet weak var fruit: UIView!
-    
     @IBOutlet weak var meat: UIView!
+    @IBOutlet weak var dairy: UIView!
+    @IBOutlet weak var grains: UIView!
+    @IBOutlet weak var veg: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        pieChart1.delegate = self
-        pieChart2.delegate = self
+        fruitChart.delegate = self
+        meatChart.delegate = self
+        grainsChart.delegate = self
+        dairyChart.delegate = self
+        vegChart.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        pieChart1.frame = CGRect(x:0, y:0, width: fruit.frame.size.width, height: fruit.frame.size.width)
-        pieChart2.frame = CGRect(x:0, y:0, width: meat.frame.size.width, height: meat.frame.size.width)
-        pieChart1.center = fruit.center
-        pieChart2.center = meat.center
-        view.addSubview(pieChart1)
-        view.addSubview(pieChart2)
         
+        // adding the location
+        fruitChart.frame = CGRect(x:0, y:0, width: fruit.frame.size.width, height: fruit.frame.size.width)
+        meatChart.frame = CGRect(x:0, y:0, width: meat.frame.size.width, height: meat.frame.size.width)
+        dairyChart.frame = CGRect(x:0, y:0, width: dairy.frame.size.width, height: dairy.frame.size.width)
+        grainsChart.frame = CGRect(x:0, y:0, width: grains.frame.size.width, height: grains.frame.size.width)
+        vegChart.frame = CGRect(x:0, y:0, width: veg.frame.size.width, height: veg.frame.size.width)
+        
+        // placing in center of view
+        fruitChart.center = fruit.center
+        meatChart.center = meat.center
+        grainsChart.center = grains.center
+        dairyChart.center = dairy.center
+        vegChart.center = veg.center
+        
+        // adding to view
+        view.addSubview(fruitChart)
+        view.addSubview(meatChart)
+        view.addSubview(grainsChart)
+        view.addSubview(dairyChart)
+        view.addSubview(vegChart)
+        
+        // creating dummy data
         var entries = [ChartDataEntry]()
         
         for x in 0..<10 {
@@ -40,11 +67,12 @@ class HomeViewController: UIViewController, ChartViewDelegate {
         let set = PieChartDataSet(entries : entries)
         set.colors = ChartColorTemplates.colorful()
         let data = PieChartData(dataSet: set)
-        pieChart1.data = data
-        pieChart2.data = data
+        
+        // adding dummy data to each pie chart
+        fruitChart.data = data
+        meatChart.data = data
+        grainsChart.data = data
+        dairyChart.data = data
+        vegChart.data = data
     }
-    
-
-   
-
 }
