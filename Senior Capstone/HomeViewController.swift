6667b32f368cd0ae1f69e490f3b35c3fd6c1acde
Emily Howell <emilythowell@gmail.com>
2020-10-16 11:32:40 -0400

    colors :)
    Signed-off-by: Emily Howell <emilythowell@gmail.com>

--- Senior Capstone/HomeViewController.swift
+++ Senior Capstone/HomeViewController.swift
@@ -78,11 +78,11 @@ class HomeViewController: UIViewController, ChartViewDelegate {
     private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
       var colors: [UIColor] = []
       for _ in 0..<numbersOfColor {
-        let red = Double(arc4random_uniform(256))
-        let green = Double(arc4random_uniform(256))
-        let blue = Double(arc4random_uniform(256))
-        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
-        colors.append(color)
+        //let red = Double(arc4random_uniform(256))
+        //let green = Double(arc4random_uniform(256))
+        //let blue = Double(arc4random_uniform(256))
+
+        colors.append(ThemeManager.randomWheelColor())
       }
       return colors
     }
