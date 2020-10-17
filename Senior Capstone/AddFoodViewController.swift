c434368c3ccd110ca94d4f7e307b4a5b58d1a4ad
Tommy Moawad <tommymoawad@MacBook-Pro-2.attlocal.net>
2020-10-17 09:37:13 -0400

    Stepper increment and decrement by 0.5

--- Senior Capstone/AddFoodViewController.swift
+++ Senior Capstone/AddFoodViewController.swift
@@ -14,23 +14,18 @@ class AddFoodViewController: UIViewController, UIPickerViewDelegate {
 
     @IBOutlet weak var nameInputBox: UITextField!
 
-    var fruitValue: Double = 0.0
     @IBOutlet weak var fruitServingStepper: UIStepper!
     @IBOutlet weak var fruitServingLabel: UILabel!
     
-    var vegValue: Double = 0.0
     @IBOutlet weak var vegServingStepper: UIStepper!
     @IBOutlet weak var vegServingLabel: UILabel!
     
-    var proteinValue: Double = 0.0
     @IBOutlet weak var proteinServingStepper: UIStepper!
     @IBOutlet weak var proteinServingLabel: UILabel!
     
-    var dairyValue: Double = 0.0
     @IBOutlet weak var dairyServingStepper: UIStepper!
     @IBOutlet weak var dairyServingLabel: UILabel!
     
-    var grainValue: Double = 0.0
     @IBOutlet weak var grainServingStepper: UIStepper!
     @IBOutlet weak var grainServingLabel: UILabel!
     
@@ -38,48 +33,30 @@ class AddFoodViewController: UIViewController, UIPickerViewDelegate {
     @IBOutlet weak var submitButton: UIButton!
 
     let appDelegate = UIApplication.shared.delegate as! AppDelegate;
-
-    /* Basically, for the UIStepper you can't tell if it's being incremented
-        or decremented unless you use this weird round about way.
-    */
-    func increment(_ sender: UIStepper, valueToUpdate: inout Double){
-        let increment: Double = 0.5
-        
-        if sender.value == 1.0 {
-            print("pos")
-            valueToUpdate += increment
-        }
-        else {
-            print("here")
-            valueToUpdate -= increment
-        }
-        sender.value = 0  //resetting the stepper value so negative is -1 and pos
-    }
     
-    func updateFoodGroup(_ sender: UIStepper, valueToUpdate: inout Double, label: UILabel){
-        increment(sender, valueToUpdate: &valueToUpdate)
-        label.text = String(valueToUpdate)
+    func updateFoodGroup(_ sender: UIStepper, label: UILabel){
+        label.text = String(sender.value)
     }
     
     @IBAction func registerFruitServingChange(_ sender: UIStepper) {
-        updateFoodGroup(sender, valueToUpdate: &fruitValue, label: fruitServingLabel)
+        updateFoodGroup(sender, label: fruitServingLabel)
     }
     
     @IBAction func registerVegServingChange(_ sender: UIStepper) {
-        updateFoodGroup(sender, valueToUpdate: &vegValue, label: vegServingLabel)
+        updateFoodGroup(sender, label: vegServingLabel)
     }
     
     @IBAction func registerProteinServingChange(_ sender: UIStepper) {
-        updateFoodGroup(sender, valueToUpdate: &proteinValue, label: proteinServingLabel)
+        updateFoodGroup(sender, label: proteinServingLabel)
     }
     
     @IBAction func registerDairyServingChange(_ sender: UIStepper) {
-        updateFoodGroup(sender, valueToUpdate: &dairyValue, label: dairyServingLabel)
+        updateFoodGroup(sender, label: dairyServingLabel)
     }
     
     
     @IBAction func registerGrainServingChange(_ sender: UIStepper) {
-        updateFoodGroup(sender, valueToUpdate: &grainValue, label: grainServingLabel)
+        updateFoodGroup(sender, label: grainServingLabel)
     }
     
     
@@ -98,11 +75,11 @@ class AddFoodViewController: UIViewController, UIPickerViewDelegate {
         
         DatabaseFunctions.insertFoodRecord(name: nameToSave,
                                            date: DailyState.todaysDate,
-                                           dairyServings: dairyValue,
-                                           fruitServings: fruitValue,
-                                           grainServings: grainValue,
-                                           proteinServings: proteinValue,
-                                           vegServings: vegValue)
+                                           dairyServings: dairyServingStepper.value,
+                                           fruitServings: fruitServingStepper.value,
+                                           grainServings: grainServingStepper.value,
+                                           proteinServings: proteinServingStepper.value,
+                                           vegServings: vegServingStepper.value)
 
         tabBarController?.selectedIndex = 0
     }
@@ -113,6 +90,12 @@ class AddFoodViewController: UIViewController, UIPickerViewDelegate {
         dateFormatter.dateStyle = DateFormatter.Style.short
         let strDate = dateFormatter.string(from: datePicker.date)
         DailyState.updateTodaysDate(todaysDate: strDate)
+        fruitServingStepper.stepValue = 0.5
+        grainServingStepper.stepValue = 0.5
+        vegServingStepper.stepValue = 0.5
+        dairyServingStepper.stepValue = 0.5
+        proteinServingStepper.stepValue = 0.5
+
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
