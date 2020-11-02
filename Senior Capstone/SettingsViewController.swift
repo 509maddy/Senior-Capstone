f9d368a23b64f4dbf43070070958a2543b840818
Ally Dinhofer <ally.dinhofer@gmail.com>
2020-11-02 16:37:15 -0500

    trying to make drop down
    Signed-off-by: Ally Dinhofer <ally.dinhofer@gmail.com>

--- Senior Capstone/SettingsViewController.swift
+++ Senior Capstone/SettingsViewController.swift
@@ -35,11 +35,46 @@ class SettingsViewController: UIViewController, ModalTransitionListener  {
     var dairyValue: Double = DailyState.dairyGoal
     
     // personal info outlets
-    
     @IBOutlet weak var height: UITextField!
     @IBOutlet weak var weight: UITextField!
     @IBOutlet weak var goalWeight: UITextField!
     
+    // sex drop down oulets
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var dropDown: UIPickerView!
+    
+    var list = ["1", "2", "3"]
+
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+
+        return list.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        self.view.endEditing(true)
+        return list[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        self.textBox.text = self.list[row]
+        self.dropDown.isHidden = true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        if textField == self.textBox {
+            self.dropDown.isHidden = false
+            //if you don't want the users to se the keyboard type:
+
+            textField.endEditing(true)
+        }
+    }
     
     @IBAction func saveSliderValues(_ sender: Any) {
 
