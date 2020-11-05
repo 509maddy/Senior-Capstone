124edf170ea19437dfe9ddadf32dc0eff11eac04
Emily Howell <emilythowell@gmail.com>
2020-11-05 01:47:33 -0500

    Added a check mark for when you have reached your daily goal. Also added greater than and less than symbols for if you have not yet reached or exceeded your daily goals

--- Senior Capstone/HomeViewController.swift
+++ Senior Capstone/HomeViewController.swift
@@ -18,34 +18,40 @@ class HomeViewController: UIViewController, ChartViewDelegate, ModalTransitionLi
     @IBOutlet weak var fruitLabelHeight: NSLayoutConstraint!
     @IBOutlet weak var fruitPaddingHeight: NSLayoutConstraint!
     @IBOutlet weak var fruitDividerHeight: NSLayoutConstraint!
-
+    @IBOutlet weak var fruitCompletedCheck: UIImageView!
+    
     @IBOutlet weak var vegetablePieChartView: PieChartView!
     @IBOutlet weak var vegetablePieChartHeight: NSLayoutConstraint!
     @IBOutlet weak var vegetableLabelHight: NSLayoutConstraint!
     @IBOutlet weak var vegetablePaddingHeight: NSLayoutConstraint!
     @IBOutlet weak var vegetableDividerHeight: NSLayoutConstraint!
-
+    @IBOutlet weak var vegetableCompletedCheck: UIImageView!
+    
     @IBOutlet weak var meatPieChartView: PieChartView!
     @IBOutlet weak var meatPieChartHeight: NSLayoutConstraint!
     @IBOutlet weak var meatLabelHeight: NSLayoutConstraint!
     @IBOutlet weak var meatPaddingHeight: NSLayoutConstraint!
     @IBOutlet weak var meatDividerHeight: NSLayoutConstraint!
+    @IBOutlet weak var meatCompletedCheck: UIImageView!
 
     @IBOutlet weak var dairyPieChartView: PieChartView!
     @IBOutlet weak var diaryPieChartHeight: NSLayoutConstraint!
     @IBOutlet weak var dairyLabelHeight: NSLayoutConstraint!
     @IBOutlet weak var dairyPaddingHeight: NSLayoutConstraint!
     @IBOutlet weak var dairyDividerHeight: NSLayoutConstraint!
+    @IBOutlet weak var dairyCompletedCheck: UIImageView!
 
     @IBOutlet weak var grainPieChartView: PieChartView!
     @IBOutlet weak var grainPieChartHeight: NSLayoutConstraint!
     @IBOutlet weak var grainLabelHeight: NSLayoutConstraint!
+    @IBOutlet weak var grainCompletedCheck: UIImageView!
 
     var goalFruit: Double = 0.0
     var goalDairy: Double = 0.0
     var goalGrain: Double = 0.0
     var goalProtein: Double = 0.0
     var goalVeg: Double = 0.0
+    
     @IBOutlet weak var navDate: UIBarButtonItem!
     
     override func viewDidLoad() {
@@ -87,11 +93,11 @@ class HomeViewController: UIViewController, ChartViewDelegate, ModalTransitionLi
         var proteinChartEntries: [ChartDataEntry] = []
         var vegChartEntries: [ChartDataEntry] = []
 
-        var totalFruitServings = 0.0
-        var totalDairyServings = 0.0
-        var totalGrainServings = 0.0
-        var totalProteinServings = 0.0
-        var totalVegServings = 0.0
+        var totalFruitServings: Double = 0.0
+        var totalDairyServings: Double = 0.0
+        var totalGrainServings: Double = 0.0
+        var totalProteinServings: Double = 0.0
+        var totalVegServings: Double = 0.0
 
         for i in 0..<foodRecords.count {
             let label = foodRecords[i].value(forKeyPath: "name") as? String
@@ -134,6 +140,12 @@ class HomeViewController: UIViewController, ChartViewDelegate, ModalTransitionLi
         customPieChart(dataEntries: &grainChartEntries, pieChartView: grainPieChartView, goalServings: goalGrain, totalServings: totalGrainServings)
         customPieChart(dataEntries: &proteinChartEntries, pieChartView: meatPieChartView, goalServings: goalProtein, totalServings: totalProteinServings)
         customPieChart(dataEntries: &vegChartEntries, pieChartView: vegetablePieChartView, goalServings: goalVeg, totalServings: totalVegServings)
+        
+        progressCheck(goalServings: goalFruit, actualServings: totalFruitServings, checkMark: fruitCompletedCheck)
+        progressCheck(goalServings: goalVeg, actualServings: totalVegServings, checkMark: vegetableCompletedCheck)
+        progressCheck(goalServings: goalProtein, actualServings: totalProteinServings, checkMark: meatCompletedCheck)
+        progressCheck(goalServings: goalDairy, actualServings: totalDairyServings, checkMark: dairyCompletedCheck)
+        progressCheck(goalServings: goalGrain, actualServings: totalGrainServings, checkMark: grainCompletedCheck)
     }
 
     func updateGoals(){
@@ -284,5 +296,19 @@ class HomeViewController: UIViewController, ChartViewDelegate, ModalTransitionLi
             colors.append(ThemeManager.pieChartColor(sliceNumber: numbersOfColor-1))
         }
       return colors
+    }
+    
+    func progressCheck(goalServings: Double, actualServings: Double, checkMark: UIImageView){
+        
+        if actualServings >= goalServings+2 {
+            //checkMark.alpha = 1
+            checkMark.image = UIImage(named: "red_greaterThan")
+        } else if actualServings >= goalServings {
+            //checkMark.alpha = 1
+            checkMark.image = UIImage(named: "green_checkMark")
+        } else {
+            //checkMark.alpha = 0
+            checkMark.image = UIImage(named: "blue_lessThan")
+        }
     }
 }
